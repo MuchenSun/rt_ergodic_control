c32c08e8dde4de524b0bd8028e007e3b3c885103
sunmch15@foxmail.com <sunmch15@gmail.com>
2020-03-02 17:01:18 -0600

    small updates for simulation object, add graph_slam_demo

--- rt_erg_lib/simulation.py
+++ rt_erg_lib/simulation.py
@@ -3,6 +3,7 @@ from matplotlib import animation
 import autograd.numpy as np
 from .utils import convert_ck2dist, convert_traj2ck
 from tqdm import tqdm
+import time
 
 class simulation():
     def __init__(self, size, t_dist, model, erg_ctrl, env, tf):
@@ -12,15 +13,21 @@ class simulation():
         self.tf = tf
         self.t_dist = t_dist
         self.model = model
+        self.exec_times = np.zeros(tf)
 
-    def start(self):
+    def start(self, report=False):
         self.log = {'trajectory': []}
         state = self.env.reset()
         for t in tqdm(range(self.tf)):
+            start_time = time.time()
             ctrl = self.erg_ctrl(state)
             state = self.env.step(ctrl)
             self.log['trajectory'].append(state)
+            self.exec_times[t] = time.time()-start_time
         print("simulation finished.")
+        if report:
+            print(self.exec_times[1:10])
+            print("mean execution time: {0:.6f}(s), standard deviation: {1:.6f}(s)".format(np.mean(self.exec_times), np.std(self.exec_times)))
 
     def plot(self, point_size=1):
         [xy, vals] = self.t_dist.get_grid_spec()
@@ -30,6 +37,7 @@ class simulation():
         ax = plt.gca()
         ax.set_aspect('equal', 'box')
         plt.show()
+        return plt.gcf()
 
     def animate(self, point_size=1, show_traj=True, rate=50):
         [xy, vals] = self.t_dist.get_grid_spec()
@@ -49,6 +57,7 @@ class simulation():
 
         anim = animation.FuncAnimation(fig, sub_animate, frames=self.tf, interval=(1000/rate), blit=True)
         plt.show()
+        return anim
 
     def path_reconstruct(self):
         xy, vals = self.t_dist.get_grid_spec()
@@ -56,4 +65,6 @@ class simulation():
         ck = convert_traj2ck(self.erg_ctrl.basis, path)
         val = convert_ck2dist(self.erg_ctrl.basis, ck, size=self.size)
         plt.contourf(*xy, val.reshape(50,50), levels=20)
+        ax = plt.gca()
+        ax.set_aspect('equal', 'box')
         plt.show()
--- tests/test1.py
+++ tests/test1.py
@@ -12,16 +12,16 @@ import matplotlib.pyplot as plt
 size = 3.0
 env = DoubleIntegrator(size=size)
 model = DoubleIntegrator(size=size)
-means = [np.array([0.5, 1.8]), np.array([2.6,1.6]), np.array([2.5, 2.5])]
+means = [np.array([0.5, 1.8]), np.array([2.6,0.6]), np.array([2.5, 2.5])]
 vars = [np.array([0.2,0.2])**2, np.array([0.2,0.2])**2, np.array([0.2,0.2])**2]
 t_dist = TargetDist(num_pts=50, means=means, vars=vars, size=size)
-erg_ctrl = RTErgodicControl(model, t_dist, horizon=30, num_basis=5, batch_size=-1)
+erg_ctrl = RTErgodicControl(model, t_dist, horizon=50, num_basis=15, batch_size=-1)
 erg_ctrl.phik = convert_phi2phik(erg_ctrl.basis, t_dist.grid_vals, t_dist.grid)
 
 """start simulation"""
-tf = 2000
+tf = 3000
 erg_ctrl_sim = simulation(size, t_dist, model, erg_ctrl, env, tf)
-erg_ctrl_sim.start()
+erg_ctrl_sim.start(report=True)
 erg_ctrl_sim.animate(point_size=5, show_traj=False)
 erg_ctrl_sim.plot(point_size=1)
 erg_ctrl_sim.path_reconstruct()
